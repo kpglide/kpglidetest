7f5bb480de98eb5cf233224fdc77bc82db5d29e1
kpglide <kpglide@gmail.com>
2014-05-06 17:50:12 -0400

    updated

--- manage.py
+++ manage.py
@@ -1,7 +1,11 @@
 from flask.ext.script import Manager
-from app import app
+from app import app, db
 
 manager = Manager(app)
+
+def make_shell_context():
+	return dict(app=app, db=db, User=User, Role=Role)
+	manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
 	manager.run()
